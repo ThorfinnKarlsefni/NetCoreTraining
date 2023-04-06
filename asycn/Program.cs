079445ac3bad98a17b8b343fd80cc2e9ee71b32d
Cheung <yangr2333@gmail.com>
2023-04-06 11:13:13 +0800

    async and sync

---
+++ asycn/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    static async Task Main()
+    {
+        string filename = @"D:\course\1.txt";
+           
+        // sync
+        File.WriteAllText(filename, @"never give up");
+        string s = File.ReadAllText(filename);
+
+        // async 
+        await File.WriteAllTextAsync(filename, @"life's little bit messy.we all make mistakes");
+        string s = await File.ReadAllTextAsync(filename);
+        Console.WriteLine(s);
+    }
+
+}
