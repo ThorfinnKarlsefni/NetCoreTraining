55574e4ab0e98df3a6f6ef7e54babe08d8bcda1a
Cheung <yangr2333@gmail.com>
2023-04-07 10:11:31 +0800

    delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Delegate 
+
+            //D1 d = F1;
+            //d();
+            //calc calc = calculate;
+            //Console.WriteLine(calc(1,2));
+
+            // use action not return data
+            // use func return data
+
+            Func<int,int,int> calc = calculate;
+            Console.WriteLine(calc(1, 8));
+
+            Action D = F1;
+            D();
+
+        }
+
+        static void F1()
+        {
+            Console.WriteLine("we all make mistakes"); 
+        }
+
+        static int calculate(int i,int a)
+        {
+            return i + a;
+        }
+
+        delegate void D1();
+        delegate int calc(int i, int n);
+    }
+}
