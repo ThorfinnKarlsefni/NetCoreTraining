c1b8b4508126892d9e06febe83c9b46fe6b55104
Cheung <402832626@qq.com>
2023-04-11 11:51:09 +0800

    di case 2

--- MainSerder/MainSerder/Program.cs
+++ MainSerder/MainSerder/Program.cs
@@ -1,17 +1,26 @@
 ﻿namespace MainSerder;
 using Microsoft.Extensions.DependencyInjection;
-using ConfigServices;
 using MailServices;
-using LogServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         ServiceCollection services = new ServiceCollection();
-        services.AddScoped<IConfigService,EnvVarConfigService>();
+        // 注册服务
+
+        // config
+        // services.AddScoped<IConfigService,EnvVarConfigService>();
+        // services.AddScoped(typeof(IConfigService),s=>new FileConfigService {FilePath = "mail.ini"});
+        services.ConsoleFileConfig("mail.ini");
+        // mail server
         services.AddScoped<IMailService, MailService>();
-        services.AddScoped<ILogProvider, ConsoleProvider>();
+
+        
+        // 让服务变得更简单
+        // 拓展方法 logs
+        //services.AddScoped<ILogProvider, ConsoleProvider>();
+        services.AddConsoleLog();
         using (var sp = services.BuildServiceProvider())
         {
             // 第一个根上的对象只能用 serviceLcaator
