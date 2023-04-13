248e6385d636fbacefb05f943b96180ff3df97b5
Cheung <402832626@qq.com>
2023-04-13 14:09:59 +0800

    logging install and use 1

---
+++ Logger/Logger/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Logger;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        ServiceCollection services = new ServiceCollection();
+        services.AddLogging(logBuilder => {
+            logBuilder.AddConsole();
+            logBuilder.SetMinimumLevel(LogLevel.Trace);
+        });
+        services.AddScoped<Test>();
+        using (var sp = services.BuildServiceProvider())
+        {
+            var loggerService = sp.GetRequiredService<Test>();
+            loggerService.TestConect();
+        }
+    }
+}
+
---
+++ Logger/Logger/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+namespace Logger
+{
+	public class Test
+	{
+		private readonly ILogger<Test> logger;
+
+		public Test(ILogger<Test> logger)
+		{
+			this.logger = logger;
+		}
+
+		public void TestConect()
+		{
+			logger.LogDebug("start. execute command");
+			logger.LogDebug("linked success");
+			logger.LogWarning("query data one");
+
+			logger.LogWarning("query data second");
+			logger.LogError("query fail");
+			Console.WriteLine("#######");
+
+			try
+			{
+				File.ReadAllText("A:/1.txt");
+				logger.LogDebug("read filed. successed");
+			}catch(Exception es)
+			{
+				logger.LogError(es,"read file error");
+			}
+			Console.Read();
+
+		}
+	}
+}
+
