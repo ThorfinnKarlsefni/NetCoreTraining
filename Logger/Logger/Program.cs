31acba62b3017af40db298a4e30e3e85b1c55724
Cheung <402832626@qq.com>
2023-04-17 23:50:57 +0800

    serivceSystem logs

--- Logger/Logger/Program.cs
+++ Logger/Logger/Program.cs
@@ -1,4 +1,5 @@
 ﻿namespace Logger;
+using SystemServices;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -15,14 +16,21 @@ class Program
         services.AddLogging(logBuilder => {
             logBuilder.AddConsole();
             logBuilder.AddNLog();
-           logBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
-
+          logBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
         });
+        // log categories
+
+        
         services.AddScoped<Test>();
+        services.AddScoped<Test2>();
         using (var sp = services.BuildServiceProvider())
         {
-            var loggerService = sp.GetRequiredService<Test>();
-            loggerService.TestConect();
+            //var loggerService = sp.GetRequiredService<Test>();
+            
+            //loggerService.TestConect();
+
+            var Test2 = sp.GetRequiredService<Test2>();
+            Test2.Test();
         }
     }
 }
---
+++ Logger/Logger/Test2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SystemServices
+{
+	public class Test2
+	{
+
+        private readonly ILogger<Test2> logger;
+
+        public Test2(ILogger<Test2> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        public void Test()
+        {
+            logger.LogDebug("start conect");
+            logger.LogWarning("conect mysql");
+            logger.LogWarning("select database");
+            logger.LogError("life's little bit messy.we all make mistake");
+        }
+       
+    }
+}
+
