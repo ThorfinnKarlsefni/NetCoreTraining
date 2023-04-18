ba2ee2c042fa0f7e5a8d9cf23dc395f3c6a95a26
Cheung <402832626@qq.com>
2023-04-18 14:46:09 +0800

    serilog

--- Logger/Logger/Program.cs
+++ Logger/Logger/Program.cs
@@ -1,27 +1,38 @@
 ﻿namespace Logger;
-using SystemServices;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
-
+using Serilog;
+using Test2;
 class Program
 {
     static void Main(string[] args)
     {
 
-
+        // log categories
         //Console.WriteLine("Hello, World!");
         ServiceCollection services = new ServiceCollection();
-        services.AddLogging(logBuilder => {
-            logBuilder.AddConsole();
-            logBuilder.AddNLog();
-          logBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
-        });
-        // log categories
+        //services.AddLogging(logBuilder =>
+        //{
+        //    logBuilder.AddConsole();
+        //    //logBuilder.AddNLog();
+        //    logBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
+        //});
 
-        
-        services.AddScoped<Test>();
+
+        // serilog
+
+
+        Log.Logger = new LoggerConfiguration()
+           .MinimumLevel.Information()
+           .WriteTo.Console()
+           .WriteTo.File("/Users/cheung/Code/net/logger/log.txt",
+               rollingInterval: RollingInterval.Day,
+               rollOnFileSizeLimit: true)
+           .CreateLogger();
+
+        //services.AddScoped<Test>();
         services.AddScoped<Test2>();
         using (var sp = services.BuildServiceProvider())
         {
--- Logger/Logger/Test2.cs
+++ Logger/Logger/Test2.cs
@@ -1,26 +1,36 @@
 ﻿using Microsoft.Extensions.Logging;
+using Serilog;
 
-namespace SystemServices
+namespace Test2
 {
 	public class Test2
 	{
 
-        private readonly ILogger<Test2> logger;
+        //private readonly ILogger<Test2> logger;
 
-        public Test2(ILogger<Test2> logger)
-        {
-            this.logger = logger;
-        }
-
+        //public Test2(ILogger<Test2> logger)
+        //{
+        //    this.logger = logger;
+        //}
 
         public void Test()
         {
-            logger.LogDebug("start conect");
-            logger.LogWarning("conect mysql");
-            logger.LogWarning("select database");
-            logger.LogError("life's little bit messy.we all make mistake");
+            Log.Information("start conect");
+            Log.Information("conect mysql");
+            Log.Information("select database");
+            Log.Information("life's little bit messy.we all make mistake");
+            User user = new User { Name="jimmy", Age= "18", Message ="life's little bit messy.we all make mistake" };
+            Log.Information("processed {@Position}",user);
+            //Log.CloseAndFlush();
         }
        
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Message { get; set; }
     }
 }
 
