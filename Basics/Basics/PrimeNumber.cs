2051862b6ab83e00cb14efd812f132d6e155b57d
Cheung <402832626@qq.com>
2023-04-28 22:57:59 +0800

    prime number

---
+++ Basics/Basics/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Basics
+{
+	public static class PrimeNumber
+	{
+		public static void Prime(int num)
+		{
+
+			//Console.WriteLine((int)Math.Sqrt(5));
+			//Console.WriteLine(3 % 2);
+			int count = 0;
+			for (int i = 2; i < num; i++)
+			{
+				bool flag = true;
+                for (int k = 2; k < i; k++)
+				{
+					if (i % k == 0)
+					{
+						flag = false;
+						break;
+					}
+				}
+
+				if (flag)
+				{
+					count++;
+				}
+			}
+
+			Console.WriteLine("\n\n{0} 以内共有{1}个质数\n", num, count);
+		}
+	}
+}
+
---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Basics;
+
+Console.WriteLine("Hello, World!");
+int num = 30;
+PrimeNumber.Prime(num);
+Console.ReadKey();
