d006920eb213e0aedac202fa5ba2eb23a8f554eb
Cheung <402832626@qq.com>
2023-04-28 15:07:38 +0800

    middlware

---
+++ Middleware/Middleware/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Map("/test", async appBuilder =>
+{
+    appBuilder.Use(async (context, next) =>
+    {
+        context.Response.ContentType = "text/hmtl";
+        await context.Response.WriteAsync("1 start<br/>");
+        await next.Invoke();
+        await context.Response.WriteAsync("1 end<br/>");
+    });
+    appBuilder.Use(async (context, next) =>
+    {
+        await context.Response.WriteAsync("2 start<br/>");
+        await next.Invoke();
+        await context.Response.WriteAsync("2 end<br/>");
+    });
+    appBuilder.Run(async ctx =>
+    {
+        await ctx.Response.WriteAsync("hello middleware<br/>");
+    });
+
+});
+
+app.Run();
+
